f2543a7b6abcb36e6133175c88fcd098d3fd555a
Yair Reynaldo <yairreynaldo@hotmail.com>
2022-10-04 22:55:06 -0300

    cambios en el home

--- TP-2/js/main.js
+++ TP-2/js/main.js
@@ -1,2 +1,25 @@
-"use strict";
+ "use strict";
+ document.addEventListener("DOMContentLoaded", () => {
 
+     const menu = document.querySelector(".menu");
+     document.querySelector(".burguer").addEventListener("click", (e) => {
+         menu.classList.toggle("active");
+     });
+     document.querySelector(".cruz").addEventListener("click", (e) => {
+         if (menu.classList.contains("active")) {
+             menu.classList.remove("active");
+         }
+     });
+
+
+     /* function despliegue() {
+         menu.classList.toggle("menuactive");
+     }
+
+     function cerrar() {
+         menu.classList.toggle("menu");
+     } */
+
+
+
+ });
