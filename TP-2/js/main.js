b3e47770bb1715c9bc8b879c2b93ea6b6fe3da41
Yair Reynaldo <yairreynaldo@hotmail.com>
2022-10-26 15:59:04 -0300

    cambio imgs de cars

--- TP-2/js/main.js
+++ TP-2/js/main.js
@@ -24,34 +24,6 @@
          }, 50);
      }
 
-     const main = document.querySelector("#main");
-     const menu = document.querySelector(".menu-burguer");
-     document.querySelector(".burguer").addEventListener("click", (e) => {
-         if (!menuPerfil.classList.contains("perfil-active")) {
-             menu.classList.toggle("burguer-active");
-         }
-     });
-     document.querySelector(".cruz").addEventListener("click", (e) => {
-         if (menu.classList.contains("burguer-active")) {
-             menu.classList.remove("burguer-active");
-         }
-     });
-
-     main.addEventListener("click", (e) => {
-         if (menu.classList.contains("burguer-active")) {
-             menu.classList.remove("burguer-active");
-         }
-         if (menuPerfil.classList.contains("perfil-active")) {
-             menuPerfil.classList.remove("perfil-active");
-         }
-     });
-
-     const menuPerfil = document.querySelector(".menu-perfil");
-     document.querySelector(".fotoperfil").addEventListener("click", (e) => {
-         if (!menu.classList.contains("burguer-active")) {
-             menuPerfil.classList.toggle("perfil-active");
-         }
-     });
 
      const itemsContenido = [...document.querySelectorAll('.itemCarrusel')];
      const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
