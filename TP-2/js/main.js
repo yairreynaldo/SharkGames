6b8f1292c55ee74a32926a7df686a9e8c5895f5f
Yair Reynaldo <yairreynaldo@hotmail.com>
2022-10-27 19:16:01 -0300

    cambio en pag lanz

--- TP-2/js/main.js
+++ TP-2/js/main.js
@@ -29,18 +29,62 @@
      const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
      const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-     itemsContenido.forEach((item, i) => {
-         let itemsDimensions = item.getBoundingClientRect();
-         let itemsWidth = itemsDimensions.width;
 
+     itemsContenido.forEach((item, i) => {
+         /* let itemsDimensions = item.getBoundingClientRect();
+         let itemsWidth = itemsDimensions.width; */
+         /* console.log(itemsContenido); */
+
+         /* preBtn[i].classList.toggle('disabled'); */
          nxtBtn[i].addEventListener('click', () => {
-             item.scrollLeft += itemsWidth;
+             item.scrollLeft += 530;
          })
 
          preBtn[i].addEventListener('click', () => {
-             item.scrollLeft -= itemsWidth;
+             item.scrollLeft -= 530;
          })
      })
+
+     /*  const itemsContenido = document.querySelectorAll('.itemCarrusel');
+      const nxtBtn = document.querySelector('.nxt-btn');
+      const preBtn = document.querySelector('.pre-btn');
+      console.log(itemsContenido);
+
+      window.addEventListener('load', () => {
+          itemsContenido[0].classList.add('active-card');
+          itemsContenido[1].classList.add('active-card');
+          itemsContenido[2].classList.add('active-card');
+          itemsContenido[3].classList.add('active-card');
+      })
+
+      nxtBtn.addEventListener('click', () => {
+          paginationTwoPagos();
+      })
+
+      preBtn.addEventListener('click', () => {
+          paginationOnePagos();
+      })
+      const paginationOnePagos = () => {
+          itemsContenido[4].classList.remove('active-card');
+          itemsContenido[5].classList.remove('active-card');
+          itemsContenido[6].classList.remove('active-card');
+          itemsContenido[7].classList.remove('active-card');
+          itemsContenido[0].classList.add('active-card');
+          itemsContenido[1].classList.add('active-card');
+          itemsContenido[2].classList.add('active-card');
+          itemsContenido[3].classList.add('active-card');
+      }
+
+      const paginationTwoPagos = () => {
+          itemsContenido[0].classList.remove('active-card');
+          itemsContenido[1].classList.remove('active-card');
+          itemsContenido[2].classList.remove('active-card');
+          itemsContenido[3].classList.remove('active-card');
+          itemsContenido[4].classList.add('active-card');
+          itemsContenido[5].classList.add('active-card');
+          itemsContenido[6].classList.add('active-card');
+          itemsContenido[7].classList.add('active-card');
+      } */
 
 
 
